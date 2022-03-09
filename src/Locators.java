3d212ba0b95c09f57b8aea79c242d1056cbbb480
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-03-09 20:42:36 +0100

    Locators2

--- src/Locators.java
+++ src/Locators.java
@@ -16,8 +16,17 @@ public class Locators {
         driver.findElement(By.id("email")).sendKeys("Iza");
         driver.findElement(By.name("password")).sendKeys("Iza");
         driver.findElement(By.className("btn-primary")).click();
+        //<div class="bodySmall m-b-3-xs text-center-xs auth-flash-error">Your email or password is incorrect.</div>
         System.out.println(driver.findElement(By.cssSelector("div.auth-flash-error")).getText());
-
+        driver.findElement(By.linkText("Forgot Password")).click();
+        driver.findElement(By.linkText("Forgot Password")).click();
+        //<input type="email" name="email" id="email" required="" autofocus="autofocus" autocomplete="email" class="form-input p-v-1-xs p-l-2-xs p-r-4-xs col-12-xs bodyMain error" aria-live="polite">
+        // //Tagname[atribute='value'][index]
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("test@gmail.com");
+        driver.findElement(By.cssSelector("input[name='email']")).clear();
+        driver.findElement(By.xpath("//input[@name='email'][1]")).sendKeys("test2@gmail.com");
+        //in css you cannot use [index]
+        //driver.findElement(By.cssSelector("input[name='email']:nth-child(1)")).clear();
 
     }
 }
