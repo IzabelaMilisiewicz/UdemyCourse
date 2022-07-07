41b75117a845128216ef91cd89da3079112b2dcf
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-07-07 16:54:06 +0200

    Next dropdown list with wile and for loop

---
+++ src/UpdatedDropdownWithLoops.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropdownWithLoops {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.findElement(By.id("divpaxinfo")).click();
+            Thread.sleep(2000);
+
+        /*int i =1;
+        while(1<5)
+        {
+            //bedzie sie dzialo dopoki bedzie prawdą
+            driver.findElement(By.id("hrefIncAdt")).click(); //4 times
+            i++;
+        }"
+        */
+
+        //a teraz to samo tylko z for loop
+
+        for(int i=1;i<5;i++){
+            driver.findElement(By.id("hrefIncAdt")).click(); //4 times
+
+        }
+
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+    }
+}
