a1d5222f9e7832a1db0cce4cc921a0228f06e9bf
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-08-04 17:31:16 +0200

    Clening the code for adding products to the cart

--- src/SynchronizationInSelenium.java
+++ src/SynchronizationInSelenium.java
@@ -1,5 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SynchronizationInSelenium {
     //implicit wait - set maximum waiting time for WHOLE PROGRAM - wait some time until element will be presented but if it will be sooner than set time, go further
@@ -10,8 +15,53 @@ public class SynchronizationInSelenium {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
+        String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
+
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        Thread.sleep(2000);
+        addItems(driver, itemsNeeded);
+//        below lines should be here if we wouldn't set "static" for class addItems below, but we did so we can use method from
+//        SynchronizationInSelenium s = new SynchronizationInSelenium();
+//        s.addItems(driver, itemsNeeded);
+        driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+        driver.findElement(By.xpath("//button[contains(text(), 'PROCEED TO CHECKOUT')]")).click();
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+
+    }
+
+
+    public static void addItems(WebDriver driver, String[] itemsNeeded) {
+        //it's reusable method this is why is out from above one
+        int j = 0;
+        List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+        for (int i = 0; i < products.size(); i++) {
+            //Brocolli - 1 Kg - this is name of product, we have to extract name of vegetable
+            String[] name = products.get(i).getText().split("-");
+            //trim() deletes empty spaces:
+            String formattedName = name[0].trim();
+
+            //check whether name is present in arrayList or not
+            //convert array (use less memory) into arrayList for easy search
+
+            List itemsNeededList = Arrays.asList(itemsNeeded);
+//            int j=0;
+
+            if (itemsNeededList.contains(formattedName)) {
+                j++;
+                //Click Add to cart
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                //we shouldn't relay on text locator because it can change like here from ADD TO CART to ADDED
+                //break; - we cannot use it because we would like to find multiple values
+                //what to do to stop the test after adding all needed items to the cart and not iterate thru other products without any sense?
+                //3 times - 3 products from list itemsNeeded have been added to the cart
+                if (j == itemsNeeded.length) {
+                    break;
+                }
+
+            }
+        }
 
     }
 }
