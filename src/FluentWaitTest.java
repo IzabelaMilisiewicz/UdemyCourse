0b0a1ed31fb87ecdae9feaa4bb036b2e7af0a129
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-08-09 18:44:29 +0200

    FluentWair example

---
+++ src/FluentWaitTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class FluentWaitTest {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("[id='start'] button")).click();
+
+        Wait wait = new FluentWait<WebDriver>(driver)  //FluentWait is our class and it is imlementing Wait interface
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(3))    //polling meand how often it should be checked
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = (WebElement) wait.until(new Function<WebDriver, WebElement>() {
+
+            public WebElement apply(WebDriver driver) {
+                if (driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()) {
+                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
+                } else
+                    return null;
+            }
+        });
+
+        System.out.println(driver.findElement(By.cssSelector("[id='finish'] h4")).getText());
+    }
+}
+
