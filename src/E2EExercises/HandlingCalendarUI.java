e6d23ffbfa1532fa918fd56d801402aea2ce2590
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-09-13 20:46:28 +0200

    Handling calendar - day and month

--- src/E2EExercises/HandlingCalendarUI.java
+++ src/E2EExercises/HandlingCalendarUI.java
@@ -13,9 +13,21 @@ public class HandlingCalendarUI {
     public static void main(String[] args) throws InterruptedException { //InterruptedException jest zwiazane ze sleepem)
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        driver.findElement(By.id("ctl00_mainContent_view_date1")).click();
+        driver.get("https://jqueryui.com/datepicker/");
+        Thread.sleep(2000);
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe.demo-frame"))); //"iframe[class='demo-frame']"
+        driver.findElement(By.id("datepicker")).click();
+
+
+        //choose the month
+        while(!driver.findElement(By.cssSelector("[class='ui-datepicker-title']")).getText().contains("November"))
+        {
+               driver.findElement(By.xpath("//a[@data-handler='next']")).click();
+        }
+
         List<WebElement> dates = driver.findElements(By.xpath("//td[@data-handler='selectDay']/a"));
+
+        //grab the common attribute, save it to list and iterate on that list
         int count = driver.findElements(By.xpath("//td[@data-handler='selectDay']/a")).size();
         System.out.println(count);
 
@@ -31,5 +43,6 @@ public class HandlingCalendarUI {
                 break;
             }
         }
+
 }
 }
