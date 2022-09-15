4c36d57fd2b3e5993a61bf51188f8ca341374a7b
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-09-15 18:06:28 +0200

    Sum up values from the table

--- src/E2EExercises/Scrolling.java
+++ src/E2EExercises/Scrolling.java
@@ -1,14 +1,18 @@
 package E2EExercises;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Scrolling {
 
 
-    public static void main(String[] args) throws InterruptedException { //InterruptedException jest zwiazane ze sleepem)
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
@@ -16,8 +20,16 @@ public class Scrolling {
         JavascriptExecutor js = (JavascriptExecutor)driver;
 
         js.executeScript("window.scrollBy(0,500)");
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 
+        //SUM UP VALUES FROM THE TABLE
+        List<WebElement> values =  driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+        int sum = 0;
+        for(int i =0;i<values.size();i++)
+        {
+            sum = sum +Integer.parseInt(values.get(i).getText());
+        }
+        System.out.println(sum);
 }
 }
