f1b3ee1a2b7f9a302a2307d2f37ede9622c3405e
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2022-10-06 18:28:19 +0200

    Another methods from chromeOptions class - set proxy, set default sownload folder, block popup windows)

--- src/HandlingHTTPDCertifications.java
+++ src/HandlingHTTPDCertifications.java
@@ -1,6 +1,11 @@
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HandlingHTTPDCertifications {
 
@@ -8,6 +13,19 @@ public class HandlingHTTPDCertifications {
 
         ChromeOptions options = new ChromeOptions();
         options.setAcceptInsecureCerts(true);
+
+        Proxy proxy =new Proxy();
+
+       // proxy.getHttpProxy("ipaddress:4444");   //ip address we should get from support team in  the company
+        //options.setCapability("proxy", proxy);
+
+        //block popup windows
+        options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
+
+        //setting default downoload directory
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("download.default_directory", "Downloads");
+        options.setExperimentalOption("prefs", prefs);
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver(options);
