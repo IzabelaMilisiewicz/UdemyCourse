1707b2f5a249b6d184253234cad9304526149d0e
IzabelaMilisiewicz <izabelamilisiewicz@gmail.com>
2023-02-06 18:45:04 +0100

    Invokoing multiple windows and tabs

--- src/Selenium4Features/InvokingMultipleWindowsOrTabs.java
+++ src/Selenium4Features/InvokingMultipleWindowsOrTabs.java
@@ -1,2 +1,30 @@
-package Selenium4Features;public class InvokingMultipleWindowsOrTabs {
+package Selenium4Features;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class InvokingMultipleWindowsOrTabs {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IzabelaMilisiewicz\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+        //open another tab and take text to copy in first tab
+        driver.switchTo().newWindow(WindowType.TAB);
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> it = handles.iterator();
+        String parentWindowId= it.next();
+        String childWindowID = it.next();
+        driver.switchTo().window(childWindowID);
+        driver.get("https://rahulshettyacademy.com/");
+        String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']")).get(1).getText();
+        driver.switchTo().window(parentWindowId);
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+        driver.quit();
+    }
 }
